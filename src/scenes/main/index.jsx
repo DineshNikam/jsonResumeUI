20e42ba47a8aba377784e79fbdce7c9b47bf0eeb
DineshNikam <dineshnikam1999@gmail.com>
2023-08-15 11:55:22 +0530

    UploadFile working

--- src/scenes/main/index.jsx
+++ src/scenes/main/index.jsx
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 import { Formik } from "formik";
 import BasicInfoForm from "./components/basics";
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import LocationField from "./components/locationFeild";
 // import WorkExperienceForm from "./WorkExperienceForm";
 import resumeSchema from "../../Data/yup";
@@ -16,8 +16,6 @@ import EducationFeild from "./components/educationFeild";
 import VolunteerFeild from "./components/volunteerFeild";
 import ArrayWithNoNesting from "../../components/arrayWithNoNesting";
 
-import debounce from "lodash.debounce"; // Import the debounce function
-
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import json from "react-syntax-highlighter/dist/esm/languages/hljs/json";
 import railscasts from "react-syntax-highlighter/dist/esm/styles/hljs/railscasts";
@@ -26,29 +24,42 @@ import ProjectsFeild from "./components/projectsFeild";
 import InterestsFeild from "./components/interestFeild";
 import { CopyButton } from "../../components/copyButton";
 import { SaveFile } from "../../components/writeResume";
+import JsonFileProcessor from "../../components/jsonFileProc";
 
-// ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥/////
+// ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥   ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥/////
+
 SyntaxHighlighter.registerLanguage("javascript", json);
+const debounce = require("lodash/debounce");
+
+//-------------------------------------------------------------------------------
 
 const JsonResumeForm = () => {
   const theme = useTheme();
   const color = tokens(theme.palette.mode);
   const [copied, setCopied] = useState(false);
+
+  const [importDataPaper, setImportDataPaper] = useState(false);
+
+  //♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
   const [jsonData, setJsonData] = useState(
     JSON.stringify(initialResumeData, null, 2)
   );
+  const debouncedUpdateJsonData = debounce((values) => {
+    setJsonData(JSON.stringify(values, null, 2));
+  }, 300);
 
+  // For preView of Data
   const handleFormChange = (values) => {
-    debounce((values) => {
-      setJsonData(JSON.stringify(values, null, 2));
-    }, 300);
+    debouncedUpdateJsonData(values); // Call the debounced function
   };
 
+  //CoppyButton OnClick
   const [open, setOpen] = useState(false);
   const handleClick = () => {
     setOpen(true);
   };
 
+  // Coppy Button handleClose
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
@@ -56,12 +67,17 @@ const JsonResumeForm = () => {
 
     setOpen(false);
   };
+
+  // ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦ Upload File And Handle Logic ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
+
+  // ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
   return (
     <Grid container spacing={2} pt={3} px={1} gap={"2"} rowGap={"50px"}>
       <Grid item xs={12} sm={12} md={6} paddingX={2} paddingY={2}>
         <Typography variant="h2">Form</Typography>
         <Box backgroundColor={color.primary[500]} pr={2}>
           <Formik
+            enableReinitialize
             initialValues={initialResumeData}
             validationSchema={resumeSchema}
             onSubmit={(values) => {
@@ -85,6 +101,52 @@ const JsonResumeForm = () => {
                   >
                     Submit
                   </Button>
+                  <Button
+                    color={"inherit"}
+                    variant="outlined"
+                    onClick={() => setImportDataPaper(true)}
+                    sx={{ marginLeft: "20px" }}
+                  >
+                    Import Data
+                  </Button>
+                  {importDataPaper && (
+                    <Box
+                      position="absolute"
+                      top="0px"
+                      left="0px"
+                      width={"100vw"}
+                      height={"100vh"}
+                      // backgroundColor="grey"
+                      zIndex={10}
+                      onClick={() => setImportDataPaper(false)}
+                    >
+                      <Paper
+                        sx={{
+                          height: "50%",
+                          weight: "50%",
+                          position: "absolute",
+                          top: "50%",
+                          left: "50%",
+
+                          margin: "auto",
+                          transform: "translate(-50%,  -50%)",
+                        }}
+                        onClick={(e) => e.stopPropagation()}
+                      >
+                        <Box
+                          margin="2rem"
+                          display="flex"
+                          flexDirection="column"
+                          alignContent="stretch"
+                          alignItems="start"
+                        >
+                          <JsonFileProcessor />
+                        </Box>
+                      </Paper>
+                    </Box>
+                  )}
+
+                  {/* Download Resume */}
                   <Button
                     color="info"
                     variant="outlined"
@@ -330,16 +392,18 @@ const JsonResumeForm = () => {
       </Grid>
       <Grid item xs={12} sm={12} md={6} paddingY={2} position="sticky">
         <Typography variant="h2">JSON Output</Typography>
-        {
-          <CopyButton
-            copied={copied}
-            jsonData={jsonData}
-            setCopied={setCopied}
-            handleClick={handleClick}
-            open={open}
-            handleClose={handleClose}
-          />
-        }
+        <Box display={"flex"} flexDirection={"row"}>
+          {
+            <CopyButton
+              copied={copied}
+              jsonData={jsonData}
+              setCopied={setCopied}
+              handleClick={handleClick}
+              open={open}
+              handleClose={handleClose}
+            />
+          }
+        </Box>
         <Box
           sx={{
             maxHeight: "80vh", // Set the maximum height for the Box
